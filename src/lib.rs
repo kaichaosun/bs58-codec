c31d1953cdaab70a048f54977cae21ba13c970d6
Kaichao Sun <kaichaosuna@gmail.com>
2020-09-03 18:25:05 +0800

    Init yew

---
+++ src/lib.rs
@@ -0,0 +1,58 @@
+use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+
+struct Model {
+    link: ComponentLink<Self>,
+    value: i64,
+}
+
+enum Msg {
+	AddOne,
+	Bs58Decode,
+	Bs58Encode,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            link,
+			value: 0,
+			decodedPeerId: "",
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::AddOne => self.value += 1
+        }
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        // This component has no properties so we will always return "false".
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
+				<p>{ self.value }</p>
+				
+				<input type="text" id="source-peer-id" value="input your peer id" />
+
+				<button onclick=self.link.callback(|_| Msg::Bs58Decode)>{ "Decode" }</button>
+				<p>{ self.encodedPeerId }</p>
+            </div>
+        }
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn run_app() {
+    App::<Model>::new().mount_to_body();
+}
